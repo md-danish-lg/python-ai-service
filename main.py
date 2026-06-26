407a6840df3da1f20825fba581a306a922b8e737
md-danish-lg <dk665226@gmail.com>
2026-06-26 14:10:05 +0500

    Add EchoItem model and echoBack endpoint to handle POST requests

--- main.py
+++ main.py
@@ -6,10 +6,18 @@ app = FastAPI()
 
 
 
+class EchoItem(BaseModel):
+    text: str
+
     
 @app.get("/")
 async def root():
     return {"message": "hello world"}
+
+
+@app.post("/echo")
+async def echoBack(item: EchoItem):
+    return item.text
 
 
 
